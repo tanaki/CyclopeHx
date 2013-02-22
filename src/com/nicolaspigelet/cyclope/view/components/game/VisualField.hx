defa94bcb5bcb2259439dee0e6aaef395f34f3d2
Nico <nico@sennep.com>
2013-02-22 18:03:11 +0000

    more

--- src/com/nicolaspigelet/cyclope/view/components/game/VisualField.hx
+++ src/com/nicolaspigelet/cyclope/view/components/game/VisualField.hx
@@ -1,4 +1,6 @@
-package com.nicolaspigelet.cyclope.view.components;
+package com.nicolaspigelet.cyclope.view.components.game;
+
+import com.nicolaspigelet.cyclope.Config;
 
 import nme.display.Sprite;
 
@@ -8,10 +10,26 @@ import nme.display.Sprite;
 class VisualField extends Sprite 
 {
 
+	private var radius : Int = 40;
+	private var offset : Int = 0;
+
 	public function new() 
 	{
 		super();
 
-		
+	}
+
+	public function update () : Void {
+
+		graphics.clear();
+		graphics.beginFill(0xff0000, 0.5);
+		graphics.moveTo( 0, 0 );
+
+		var pointX = -Config.STAGE_WIDTH * 0.5 + offset;
+
+		graphics.lineTo( pointX - radius, Config.STAGE_HEIGHT * 0.5 );
+		graphics.lineTo( pointX + radius, Config.STAGE_HEIGHT * 0.5 );
+
+		offset++;
 	}
 }
